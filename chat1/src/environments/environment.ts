3217ce5f57ce554ae48580d16c0ff89bac4734f5
MsMapple <81583351+MsMapple@users.noreply.github.com>
2022-10-11 09:41:42 -0300

    firebase

--- chat1/src/environments/environment.ts
+++ chat1/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'profe-danzas',
+    appId: '1:861833113787:web:dfe547502c4d6a950ef99e',
+    storageBucket: 'profe-danzas.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAFF9-OcEM0YjOQgNKwDTO0EtuNheEBwrc',
+    authDomain: 'profe-danzas.firebaseapp.com',
+    messagingSenderId: '861833113787',
+    measurementId: 'G-8C1WK4S6Q4',
+  },
   production: false
 };
 
